5348b24d19cd1ec9c0160e8d3f65aa36737b86cd
Ksu <oksana.ivanova.fd@gmail.com>
2021-04-11 20:08:47 +0300

    master

---
+++ Ivanova_19/custom.js
@@ -0,0 +1,22 @@
+let btnOpen = document.querySelector('button');
+let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+width=500,height=500`;
+
+btnOpen.addEventListener('click', openWindow);
+
+function openWindow() {
+    let newWin = window.open("", "Frontend", params);
+    newWin.document.write('<title>Frontend</title><img src="./cat.jpeg" alt="cat" width="400" height="400">');
+    let timerId = setInterval(randomStep, 1000);
+
+    function randomStep() {
+        let xPosition = Math.random() * window.innerWidth;
+        let yPosition = Math.random() * window.innerHeight;
+        newWin.moveTo(xPosition, yPosition);
+    }
+
+    setTimeout(() => {
+        clearInterval(timerId);
+        newWin.close();
+    }, 15000);
+}
