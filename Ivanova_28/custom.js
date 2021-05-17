096ac80cd303048afae197dc5679c76cd487cb41
Ksu <oksana.ivanova.fd@gmail.com>
2021-05-17 14:55:51 +0300

    master

--- Ivanova_28/custom.js
+++ Ivanova_28/custom.js
@@ -1,10 +1,18 @@
 let itemCard = document.querySelectorAll('.cart_numb_item');
         for (let item of itemCard) {
-            item.addEventListener('input', function (e) {
-                if (e.data === null) {
-                    this.previousElementSibling.focus();
-                } else {
-                    this.nextElementSibling.focus();
-                }
-    })
+            item.addEventListener('input', nextItem);
+            item.addEventListener('keydown', previousItem)
+        }
+
+function nextItem(e) {
+    this.value = this.value.replace(/[^\d]/g, '');
+    if (e.target.value) {
+        e.target.nextElementSibling.focus();
+    }
+}
+
+function previousItem(e) {
+    if (e.keyCode === 8 && e.target.value.length === 0) {
+        e.target.previousElementSibling.focus();
+    }
 }
